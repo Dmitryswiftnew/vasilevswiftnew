0adc5fdea660785d57901e1e36b3d8186f65f558
Dmitryswiftnew <vasilevswiftnew@gmail.com>
2024-08-28 12:38:01 +0300

    add addThirdViewController

---
+++ RSSchool1/ThirdViewController.h
@@ -0,0 +1,16 @@
+//
+//  ThirdViewController.h
+//  RSSchool1
+//
+//  Created by Dmitry on 28.08.24.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ThirdViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ RSSchool1/ThirdViewController.m
@@ -0,0 +1,31 @@
+//
+//  ThirdViewController.m
+//  RSSchool1
+//
+//  Created by Dmitry on 28.08.24.
+//
+
+#import "ThirdViewController.h"
+
+@interface ThirdViewController ()
+
+@end
+
+@implementation ThirdViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
