bb13fa54ad461dba880447d49e8fe68f427e0a90
Dmitryswiftnew <vasilevswiftnew@gmail.com>
2024-08-28 12:44:42 +0300

    4ViewController

---
+++ RSSchool1/4ViewController.h
@@ -0,0 +1,16 @@
+//
+//  4ViewController.h
+//  RSSchool1
+//
+//  Created by Dmitry on 28.08.24.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface _ViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ RSSchool1/4ViewController.m
@@ -0,0 +1,31 @@
+//
+//  4ViewController.m
+//  RSSchool1
+//
+//  Created by Dmitry on 28.08.24.
+//
+
+#import "4ViewController.h"
+
+@interface _ViewController ()
+
+@end
+
+@implementation _ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
